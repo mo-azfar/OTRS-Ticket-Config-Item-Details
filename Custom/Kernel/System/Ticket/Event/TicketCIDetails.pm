ff9bbb4cec1fea942d2a852cab1f684f6f4329ab
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2020-07-11 03:12:02 +0800

    Update TicketCIDetails.pm

--- Custom/Kernel/System/Ticket/Event/TicketCIDetails.pm
+++ Custom/Kernel/System/Ticket/Event/TicketCIDetails.pm
@@ -1,3 +1,6 @@
+# --
+#
+# --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (GPL). If you
 # did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
@@ -44,13 +47,13 @@ sub Run {
 	my $Source = $Param{New}->{'SourceDF'}; ##This one if using GenericAgent ticket event
 	my $Destination = $Param{New}->{'DestinationDF'}; ##This one if using GenericAgent ticket event
 	
-	#get ticket object
 	my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
-	#get general catalog object
 	my $GeneralCatalogObject = $Kernel::OM->Get('Kernel::System::GeneralCatalog');
-	#get config item asset object
 	my $ConfigItemObject = $Kernel::OM->Get('Kernel::System::ITSMConfigItem');
+	my $CustomerCompanyObject = $Kernel::OM->Get('Kernel::System::CustomerCompany');
+	my $CustomerUserObject = $Kernel::OM->Get('Kernel::System::CustomerUser');
 	my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+	
 	# get ticket content
 	my %Ticket = $TicketObject->TicketGet(
         TicketID => $TicketID ,
@@ -75,58 +78,98 @@ sub Run {
 		);
 				
 		my $XMLData = $LastVersion->{XMLData}->[1]->{Version}->[1];
+		#print "Content-type: text/plain\n\n";
+	    #print Dumper($XMLData); 
+			  
 		my $Data1;
 		#search in xml definition to get the CI Name based on CI Key (passed from configuration)
 		#up to 3rd level definition
 		foreach my $ImportedCIValue (@ImportedCIValues)
 		{ 
-		  my($Class, $Definition) = split(/::/, $ImportedCIValue, 2);
-		  next if $Class ne $LastVersion->{Class};
+			my($Class, $Definition) = split(/::/, $ImportedCIValue, 2);
+		  	next if $Class ne $LastVersion->{Class};
 		  
-		  if ($Definition =~ m/::/) 
-		  { 
-		    my @array = split('::', $Definition); #split definition by :: into array for searching in xml data
-		    my @new_array = grep {$_ ne '1' && $_ ne '2' && $_ ne '3'} @array; #remove number from array for searching in xml definition
+		  	if ($Definition =~ m/::/) 
+		  	{ 
+		    	my @array = split('::', $Definition); #split definition by :: into array for searching in xml data
+		    	my @new_array = grep {$_ ne '1' && $_ ne '2' && $_ ne '3'} @array; #remove number from array for searching in xml definition
 		    
-		    if (scalar @new_array eq '1') #defintion 1st level
-		    { 
-		      no warnings qw(uninitialized);
-		      my ($hash_ref) = grep {$_->{Key} eq "$new_array[0]" } @{$LastVersion->{XMLDefinition}};
+		    	if (scalar @new_array eq '1') #defintion 1st level
+		    	{ 
+		      		no warnings qw(uninitialized);
+		      		my ($hash_ref) = grep {$_->{Key} eq "$new_array[0]" } @{$LastVersion->{XMLDefinition}};
 		              
-		      #get xml value
-		      #print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content};
-		      #print $XMLData->{InstallDate}->[1]->{Content};
-		      $Data1 .= "[$hash_ref->{Name}]: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content}\n";
-		          
-		    }
-		    elsif (scalar @new_array eq '2') #defintion 2nd level
-		    {
-		      no warnings qw(uninitialized);
-		      my ($hash_ref) = grep { $_->{Sub}->[0]->{Key} eq "$new_array[1]" } @{$LastVersion->{XMLDefinition}};
-		      
-		      #get xml value
-		      #print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{Content};
-		      #print  $XMLData->{InstallDate}->[1]->{By}->[1]->{Content};
-		      $Data1 .= "*$hash_ref->{Sub}->[0]->{Name}: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{Content}\n";
-		        
-		    }
-		    elsif (scalar @new_array eq '3') #defintion 3rd level
-		    {
-		      no warnings qw(uninitialized);
-		      my ($hash_ref) = grep { $_->{Sub}->[0]->{Sub}->[0]->{Key} eq "$new_array[2]" } @{$LastVersion->{XMLDefinition}};
-		      
-		      #get xml value
-		      #print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{$array[4]}->[$array[5]]->{Content};
-		      #print $XMLData->{InstallDate}->[1]->{By}->[1]->{Backup}->[1]->{Content};
-		      $Data1 .= "**$hash_ref->{Sub}->[0]->{Sub}->[0]->{Name}: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{$array[4]}->[$array[5]]->{Content}\n";
-		      
-		    }
-		  } 
-		  else
-		  {
-		    $Data1 .= "[$Definition] : $LastVersion->{$Definition}\n";
-		  }
-		
+		      		#get xml value
+		      		#print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content};
+		      		#print $XMLData->{InstallDate}->[1]->{Content};
+
+			  		#check config item type
+			  		if ($hash_ref->{Input}->{Type} eq "GeneralCatalog")
+					{
+						my $GeneralCatalogGet = $GeneralCatalogObject->ItemGet(ItemID =>"$XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content}",);
+						$Data1 .= "[$hash_ref->{Name}]: $GeneralCatalogGet->{Name}\n";
+					}
+					elsif ($hash_ref->{Input}->{Type} eq "Customer")
+					{
+						my %CustomerUserData = $CustomerUserObject->CustomerUserDataGet(User => "$XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content}",);
+						$Data1 .= "[$hash_ref->{Name}]: $CustomerUserData{UserFirstname} $CustomerUserData{UserLastname}\n";
+					}
+					elsif ($hash_ref->{Input}->{Type} eq "CustomerCompany")
+					{
+						my %CustomerCompanyGet = $CustomerCompanyObject->CustomerCompanyGet(CustomerID => "$XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content}",);
+						$Data1 .= "[$hash_ref->{Name}]: $CustomerCompanyGet{CustomerCompanyName}\n";
+					}
+					else
+					{
+		      			$Data1 .= "[$hash_ref->{Name}]: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{Content}\n";
+					} 
+		    	}
+		    	elsif (scalar @new_array eq '2') #defintion 2nd level
+		    	{
+		      		no warnings qw(uninitialized);
+		      		my ($hash_ref) = grep { $_->{Sub}->[0]->{Key} eq "$new_array[1]" } @{$LastVersion->{XMLDefinition}};
+		      	
+		      		#get xml value
+		      		#print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{Content};
+		      		#print  $XMLData->{InstallDate}->[1]->{By}->[1]->{Content};
+	   	
+					#check config item type
+			  		if ($hash_ref->{Sub}->[0]->{Input}->{Type} eq "GeneralCatalog")
+					{
+						my $GeneralCatalogGet = $GeneralCatalogObject->ItemGet(ItemID =>"$XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{Content}",);
+						$Data1 .= "*$hash_ref->{Sub}->[0]->{Name}: $GeneralCatalogGet->{Name}\n";
+					}
+					else
+					{
+		      			$Data1 .= "*$hash_ref->{Sub}->[0]->{Name}: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{Content}\n";
+					} 
+				
+		    	}
+		    	elsif (scalar @new_array eq '3') #defintion 3rd level
+		    	{
+		      		no warnings qw(uninitialized);
+		      		my ($hash_ref) = grep { $_->{Sub}->[0]->{Sub}->[0]->{Key} eq "$new_array[2]" } @{$LastVersion->{XMLDefinition}};
+		      	
+		      		#get xml value
+		      		#print $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{$array[4]}->[$array[5]]->{Content};
+		      		#print $XMLData->{InstallDate}->[1]->{By}->[1]->{Backup}->[1]->{Content};
+	
+			  		#check config item type
+			  		if ($hash_ref->{Sub}->[0]->{Sub}->[0]->{Input}->{Type} eq "GeneralCatalog")
+					{
+						my $GeneralCatalogGet = $GeneralCatalogObject->ItemGet(ItemID =>"$XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{$array[4]}->[$array[5]]->{Content}",);
+						$Data1 .= "**$hash_ref->{Sub}->[0]->{Sub}->[0]->{Name}: $GeneralCatalogGet->{Name}\n";
+					}
+					else
+					{
+		      			$Data1 .= "**$hash_ref->{Sub}->[0]->{Sub}->[0]->{Name}: $XMLData->{$hash_ref->{Key}}->[$array[1]]->{$array[2]}->[$array[3]]->{$array[4]}->[$array[5]]->{Content}\n";
+					} 
+				}
+		  	} 
+		  	else
+		  	{
+		  		$Data1 .= "[$Definition] : $LastVersion->{$Definition}\n";
+		  	}
 		}
 	
 		##REF  http://doc.otrs.com/doc/api/otrs/6.0/Perl/Kernel/System/DynamicFieldValue.pm.html#ValueSet (OTRS 6)
